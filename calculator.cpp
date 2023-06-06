0784114f0c682965d5527491eeeec05d592d24cf
guantanamobosch <luketyson17@gmail.com>
2023-06-06 14:17:29 -0600

    write prompt flow for the command line. write calculate function to filter inputs through conditions

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 using namespace std;
 
+// Initialize signed ints
+signed int operandOne, operandTwo;
+char operation;
+
+int calculate(int, int, char);
+
 int main()
 {
+
+    cout << "Please enter your first operand:\n";
+    cin >> operandOne;
+    cout << "Please enter your operator:\n";
+    cin >> operation;
+    cout << "Please enter your second operand:\n";
+    cin >> operandTwo;
+
+    cout << operandOne << " " << operation << " " << operandTwo << " = \n";
+    calculate(operandOne, operandTwo, operation);
+
+    return 0;
+}
+
+int calculate(int operandOne, int operandTwo, char op)
+{
+    if (op == '/')
+    {
+        cout << operandOne / operandTwo;
+    }
+    else if (op == '+')
+    {
+        cout << operandOne + operandTwo;
+    }
+    else if (op == '-')
+    {
+        cout << operandOne - operandTwo;
+    }
+    else if (op == '*')
+    {
+        cout << operandOne * operandTwo;
+    }
+
+    return 0;
 }
