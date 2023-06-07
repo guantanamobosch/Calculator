4c550e51ad6ea91d1895fe4f93eac6c3b6960362
guantanamobosch <luketyson17@gmail.com>
2023-06-07 17:49:43 -0600

    Another method for the calculator using a switch statement instead of chained conditional statements

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Command-Line Calculator
 
-This repository is for a basic arithmetic calculator written in C++. To start, the calculator only takes in two numbers and an operation via a prompt.
+This repository is for a basic arithmetic calculator written in C++. For now, the calculator only takes in two numbers and an operation via a command-line prompt.
--- calculator.cpp
+++ calculator.cpp
@@ -1,46 +1,104 @@
+// #include <iostream>
+// using namespace std;
+
+// signed int operandOne, operandTwo; // Initialize signed ints
+// char operation;                    // Initialize operation character (+, -, /, *)
+
+// int calculate(int, int, char);
+
+// int main()
+// {
+
+//     cout << "Please enter your first operand:\n";
+//     cin >> operandOne;
+//     cout << "Please enter your operator:\n";
+//     cin >> operation;
+//     cout << "Please enter your second operand:\n";
+//     cin >> operandTwo;
+
+//     cout << operandOne << " " << operation << " " << operandTwo << " = \n";
+//     calculate(operandOne, operandTwo, operation);
+
+//     return 0;
+// }
+
+// int calculate(int operandOne, int operandTwo, char op)
+// {
+//     if (op == '/')
+//     {
+//         cout << operandOne / operandTwo;
+//     }
+//     else if (op == '+')
+//     {
+//         cout << operandOne + operandTwo;
+//     }
+//     else if (op == '-')
+//     {
+//         cout << operandOne - operandTwo;
+//     }
+//     else if (op == '*')
+//     {
+//         cout << operandOne * operandTwo;
+//     }
+
+//     return 0;
+// }
+
+// --------------------
+// Another method
+
 #include <iostream>
 using namespace std;
 
-// Initialize signed ints
-signed int operandOne, operandTwo;
-char operation;
-
-int calculate(int, int, char);
-
 int main()
 {
+    // Initialize variables
+    char op;
+    double num1, num2, result;
 
-    cout << "Please enter your first operand:\n";
-    cin >> operandOne;
-    cout << "Please enter your operator:\n";
-    cin >> operation;
-    cout << "Please enter your second operand:\n";
-    cin >> operandTwo;
+    // Prompt user to enter their operator...
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
 
-    cout << operandOne << " " << operation << " " << operandTwo << " = \n";
-    calculate(operandOne, operandTwo, operation);
+    // ...and to enter their numbers
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
 
-    return 0;
-}
+    // Switch statement takes in user's operator as its expression
+    switch (op)
+    {
+    // If op = '+', then add...
+    case '+':
+        result = num1 + num2;
+        break;
+    // ...subtract...
+    case '-':
+        result = num1 - num2;
+        break;
+    // ...multiply...
+    case '*':
+        result = num1 * num2;
+        break;
+    // ...or divide.
+    case '/':
+        // Num2 cannot be zero...
+        if (num2 != 0)
+            result = num1 / num2;
+        // If it is, then this catches it.
+        else
+        {
+            cout << "Error: Division by zero is not allowed." << endl;
+            return 0;
+        }
+        break;
+    // Default case for if the user enters anything but the permitted values for op
+    default:
+        cout << "Error: Invalid operator." << endl;
+        return 0;
+    }
 
-int calculate(int operandOne, int operandTwo, char op)
-{
-    if (op == '/')
-    {
-        cout << operandOne / operandTwo;
-    }
-    else if (op == '+')
-    {
-        cout << operandOne + operandTwo;
-    }
-    else if (op == '-')
-    {
-        cout << operandOne - operandTwo;
-    }
-    else if (op == '*')
-    {
-        cout << operandOne * operandTwo;
-    }
+    // Output the result!
+    cout << "Result: " << result << endl;
 
     return 0;
 }
